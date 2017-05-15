cc51455f26167abfbe358491cdbdcf88a50537b5
limbo <limbo_china@163.com>
2017-05-16 00:07:05 +0800

    update

--- src/system.h
+++ src/system.h
@@ -33,8 +33,8 @@ typedef struct SystemStr
 
    	Atom* atoms;          // 存储原子的相关信息数据
 
-   	char* smBuf ;	// 共享缓冲区起始地址
-	char* usrBuf;
+   	char* smbuf ;	// 共享缓冲区起始地址
+	char* usrbuf;
 		
 	MPI_Win win1;	// 窗口1
 	MPI_Win win2;	// 窗口2
