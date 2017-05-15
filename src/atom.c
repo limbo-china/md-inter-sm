3b6d63f8ce1e5c6e2b49307a0ebf5430691e772e
limbo <limbo_china@163.com>
2017-05-16 01:45:01 +0800

    update

--- src/atom.c
+++ src/atom.c
@@ -516,12 +516,13 @@ void processSmData(struct SystemStr* sys, void *smbuf, enum Neighbor dimen){
             momenta[i] = buffer[num].momenta[i];
         }
         id = buffer[num].id;
-        
-         if(getMyRank()==2){
-            printf("num :%d \n",num );
-            printf("pos: %g,%g,%g\n",pos[0],pos[1],pos[2] );
-            printf("momenta: %g,%g,%g\n",momenta[0],momenta[1],momenta[2] );
-        }  
+
+        //  if(getMyRank()==2){
+        //     printf("num :%d \n",num );
+        //     printf("pos: %g,%g,%g\n",pos[0],pos[1],pos[2] );
+        //     printf("momenta: %g,%g,%g\n",momenta[0],momenta[1],momenta[2] );
+        // }  
+        assignAtom(id, pos, sys, momenta);
     }
      //printf("rank:%d test2\n ",getMyRank());
 }
